9cec734907ac288fd7dbacb87e07f734930431aa
Thalles José <thallesjosesv@gmail.com>
2025-08-06 16:32:38 -0300

    update 1.2.1

--- src/app.js
+++ src/app.js
@@ -15,10 +15,4 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.delete("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros.splice(index, 1);
-  res.status(200).send("livro removido com sucesso");
-});
-
 export default app;
